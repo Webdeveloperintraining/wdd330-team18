350f2be2063d3faa8c678f9e802097337b1f8c25
Webdeveloperintraining <sil22006@byui.edu>
2023-09-23 14:31:49 -0700

    Reverting to last working version on Sep 18

--- src/js/product.js
+++ src/js/product.js
@@ -1,21 +1,7 @@
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
-import ProductDetails from "./ProductDetails.mjs";
-import { setLocalStorage, getParams } from "./utils.mjs";
-
-const productId = getParams("product");
-const dataSource = new ProductData("tents");
-
-const product = new ProductDetails(productId, dataSource);
-product.init();
-
-/*
-import ProductData from "./ProductData.mjs";
-import ProductDetails from './ProductDetails.mjs';
-import { setLocalStorage, getLocalStorage, getParams } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParams('product');
-console.log(dataSource.findProductById(productId));
 
 // Get products currently in cart, if cart is empty initialize an array
 const cartItems = getLocalStorage("so-cart") || [];
@@ -31,8 +17,8 @@ async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
   addProductToCart(product);
 }
+
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
-*/
