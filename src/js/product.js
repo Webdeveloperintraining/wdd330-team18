b1cbd685e6622e6b5f4e86469b5e0d49283cb868
wallacejoe <JAWcoder@gmail.com>
2023-09-23 20:08:25 -0700

    W02 individual assignment. Items in cart task.

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,10 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getNumFromCart, getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
-// Get products currently in cart, if cart is empty initialize an array
-const cartItems = getLocalStorage("so-cart") || [];
-
-function addProductToCart(product) {
-  // Add new product to cart
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productId, dataSource);
+product.init();
+getNumFromCart();
