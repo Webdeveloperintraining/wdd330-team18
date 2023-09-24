71f97b234fa7c8b35be3fc2eff891f140fe307ff
Webdeveloperintraining <sil22006@byui.edu>
2023-09-23 17:16:38 -0700

    cart HTML error message solved

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,13 +1,19 @@
+
 import { getLocalStorage } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
-  const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+    if(cartItems  != null) {
+    //console.log('The variable is undefined or null');
+    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
+    document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  } else{
+    console.log("No Items added to the Shopping Cart");
+  }
 }
 
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
+  const newItem = `<li class="cart-card divider" id="${item.Id}" >
   <a href="#" class="cart-card__image">
     <img
       src="${item.Image}"
@@ -20,9 +26,30 @@ function cartItemTemplate(item) {
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
   <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
+  <span class="x-btn" onclick= "removeItem('${item.Id}')" >X</span>
 </li>`;
-
+  //getCartProductsId(item.Id); data-id="${item.Id}"
+  /*if (!items.includes(item)){
+    items.push(item);*/
+  //}
   return newItem;
 }
-
 renderCartContents();
+
+//function getCartProductsId(item){
+  //let items = array();
+  /*if (!items.includes(item)){
+      items.push(item);
+  }*/
+
+//}
+
+/*
+function removeItem(id){
+  var element = document.getElementById(`#${id}`);
+  console.log(element);
+  if (element != null) 
+  element.remove();
+
+}*/
+
